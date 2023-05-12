eebff0edcaeb6c129b25f0847d291c395701a0cc
maybeyann <133187037+maybeyann@users.noreply.github.com>
2023-05-12 10:10:47 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -170,7 +170,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: '![图片](/timg.jpg)欢迎来聊聊[wxlink](www.baidu.com)',
+    BotHello: "有什么可以帮你的吗",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) =>
